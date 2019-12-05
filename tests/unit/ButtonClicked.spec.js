26836c9b1fe58d72147430e7ab38f4e436ff58dc
aveliklaos <aveliklaos1@gmail.com>
2019-12-05 21:43:24 +0200

    Finished task 3

---
+++ tests/unit/ButtonClicked.spec.js
@@ -0,0 +1,15 @@
+// Import the mount() method from the test utils
+// and the component you want to test
+import { mount } from '@vue/test-utils'
+import Footer from "../../src/components/Footer.vue";
+
+describe('"+" button clicked in Footer', () => {
+    // Now mount the component and you have the wrapper
+    const wrapper = mount(Footer);
+
+    // Check weather the "open" property in Footer component changes to true when "+" button is clicked in Footer component
+    it('"open" property in Footer changes to true when clicked', () => {
+        wrapper.find('span').trigger('click');
+        expect(wrapper.vm.open).toEqual(true);
+    })
+});
